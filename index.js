c2a9b701c513b946a93a1f2cf1eb26c7e53171e7
Bernadette Davis <bsd231@nyu.edu>
2020-02-01 00:47:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function testFunc() {
 
 function superbowlWin(array){
   let result = array.find(element => element.result === "W");
-  return ((typeof result === 'undefined') ? undefined : result.year)
+  return ((typeof result === 'undefined') ? undefined : result.year);
 }
